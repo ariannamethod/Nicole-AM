c278376642e256b271ae9bfdd0bcebc15bd25925
Oleg <theariannamethod@gmail.com>
2025-07-01 00:12:32 +0300

    Create __init__.py

---
+++ Nicole/models/__init__.py
@@ -0,0 +1,7 @@
+from .processing_deepseek_vl_v2 import DeepseekVLV2Processor
+from .modeling_deepseek_vl_v2 import DeepseekVLV2ForCausalLM
+
+__all__ = [
+    "DeepseekVLV2Processor",
+    "DeepseekVLV2ForCausalLM",
+]
